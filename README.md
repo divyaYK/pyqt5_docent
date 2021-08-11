d0dd17e68df18a23fc7c379cfc7ac8943e27fd6b
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-08-11 22:44:23 +0530

    labels done

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Understanding PyQt5
 My own guide
 <hr>
+
+Contents Include:
+- Simple Window Creation
+- Label Creation
---
+++ labels.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+
+class Window(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.setGeometry(50, 50, 640, 480)  # x, y and h, w
+    self.setWindowTitle("Simple Window")
+    self.UI()
+
+  def UI(self):
+    text1 = QLabel("Label 1", self)
+    text2 = QLabel("Label 2", self)
+    text1.move(100, 50)
+    text2.move(200, 50)
+    self.show()
+
+def main():
+  app = QApplication(sys.argv)
+  window = Window()
+  sys.exit(app.exec_())
+
+if __name__=='__main__':
+  main()
